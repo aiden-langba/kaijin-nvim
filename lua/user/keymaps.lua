1d6ed5c99e80466d5084aad0dce9b11ac08ac940
aiden <aiden.langba@sonicbolt.com>
2022-05-30 14:01:08 +0530

    📦NEW: Nvim-tree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -73,3 +73,25 @@ keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",opts)
 keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",opts)
 keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",opts)
 keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",opts)
+
+-- nvim tree 🌲
+keymap("n", "<C-n>", ":NvimTreeToggle<CR>",opts)
+keymap("n", "<leader>r", ":NvimTreeRefresh<CR>",opts)
+keymap("n", "<leader>n", ":NvimTreeFindFile<CR>",opts)
+
+-- nnoremap <C-n> :NvimTreeToggle<CR>
+-- nnoremap <leader>r :NvimTreeRefresh<CR>
+-- nnoremap <leader>n :NvimTreeFindFile<CR>
+-- " More available functions:
+-- " NvimTreeOpen
+-- " NvimTreeClose
+-- " NvimTreeFocus
+-- " NvimTreeFindFileToggle
+-- " NvimTreeResize
+-- " NvimTreeCollapse
+-- " NvimTreeCollapseKeepBuffers
+
+-- set termguicolors " this variable must be enabled for colors to be applied properly
+
+-- " a list of groups can be found at `:help nvim_tree_highlight`
+-- highlight NvimTreeFolderIcon guibg=blue
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,6 +92,14 @@ use {
   requires = "kyazdani42/nvim-web-devicons"
 }
 
+-- explorer
+use {
+  'kyazdani42/nvim-tree.lua',
+  requires = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icon
+  },
+  -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
 
 
   -- cmp plugins
